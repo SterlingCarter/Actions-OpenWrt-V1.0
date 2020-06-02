bc4e1e83ca0e1872de8c86ded83cfefc7735e46a
SterlingCarter <66172871+SterlingCarter@users.noreply.github.com>
2020-06-02 15:30:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git SterlingCarter https://github.com/SterlingCarter/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git openwrt-packages https://github.com/SterlingCarter/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/SterlingCarter/small' feeds.conf.default
